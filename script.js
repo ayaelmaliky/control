19bb29736c150f64afe8bac3583e457a39ae0109
Aya Elmaliky <ayaelmalki2004@gmail.com>
2026-05-12 13:46:19 +0200

    js

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+
+const btn = document.getElementById("btn");
+const message = document.getElementById("message");
+
+btn.addEventListener("click", function () {
+    message.textContent = "Bienvenue sur mon portfolio 🎉";
+});
